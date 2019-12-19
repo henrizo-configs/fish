6ebe7785bbccad9ba2f1d42bd64b4a89ffb89876
Matheus <matheus.hs97@gmail.com>
2019-12-19 19:07:59 -0300

    Added updatepips.

--- config.fish
+++ config.fish
@@ -15,17 +15,20 @@ set fish_greeting ""
 
 # <abreviations>
 
-# xclip clipboard
+# xclip clipboard setting and getting
 abbr setclip "xclip -selection c"
 abbr getclip "xclip -selection c -o"
 
 # GPG symmetric basic encryption not caching pass
-abbr gpgencrypt gpg2 --no-symkey-cache -c
+abbr gpgencrypt "gpg2 --no-symkey-cache -c"
 
 # GPG symmetric encryption not caching pass: 
-abbr gpgdecrypt gpg2 --no-symkey-cache
+abbr gpgdecrypt "gpg2 --no-symkey-cache"
 
-# Clear GPG stored passwords:
-abbr gpgclear gpg-connect-agent reloadagent /bye
+# Clears GPG stored passwords:
+abbr gpgclear "gpg-connect-agent reloadagent /bye"
+
+# Updates all pip packages
+abbr updatepips "pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
 
 # </abreviations>
